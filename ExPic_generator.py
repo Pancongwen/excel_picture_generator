862fa92e99f571ef65392bdaf1521f6ab080295c
developer <developer@192.168.2.171>
2019-09-30 18:02:31 +0800

    feat: init

--- ExPic_generator.py
+++ ExPic_generator.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import sys
+import openpyxl
 from PIL import Image
 
 class Generator():
@@ -13,6 +14,8 @@ class Generator():
         self.image_width = self.image.size[0]
         self.image_height = self.image.size[1]
         self.image_pix = self.image.load()
+        self.workbook = openpyxl.Workbook()
+        self.worksheet = self.workbook.active
 
     def rgb2hex(self, rgb):
         """
@@ -20,10 +23,25 @@ class Generator():
         hex_value = '#%02x%02x%02x' % rgb
         return hex_value
 
+    def colnum_string(self, n):
+        string = ""
+        while n > 0:
+            n, remainder = divmod(n - 1, 26)
+            string = chr(65 + remainder) + string
+        return string
+
     def draw(self):
         """
         """
-        pass
+        for row in range(0, self.image_height):
+            for col in range(0, self.image_width):
+                color_rgb = self.image_pix[col, row]
+                color_hex = self.rgb2hex(color_rgb)[1:]
+                cell = self.worksheet.cell(row=row+1, column=col+1)
+                self.worksheet.row_dimensions[row+1].height = 5
+                self.worksheet.column_dimensions[self.colnum_string(col+1)].width = 5
+                cell.fill = openpyxl.styles.PatternFill("solid", fgColor=color_hex)
+        self.workbook.save("test.xlsx")
 
 
 
